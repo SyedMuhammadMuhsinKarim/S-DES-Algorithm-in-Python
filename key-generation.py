ad1d2fab96992ae1c29efee9609c8b32b0db9805
Syed Muhammad Muhsin Karim <37833481+SyedMuhammadMuhsinKarim@users.noreply.github.com>
2020-03-22 04:04:07 +0500

    Update key-generation.py

--- key-generation.py
+++ key-generation.py
@@ -1,47 +1,39 @@
 import numpy as np
 
 def table_shift(array, table_array):
-  array_shifted = np.zeros(table_array.shape[0], dtype='int')
-
-  for index, value in enumerate(table_array):
-    array_shifted[index] = array[value-1]
-
-  return array_shifted
+    array_shifted = np.zeros(table_array.shape[0], dtype='int')
+    for index, value in enumerate(table_array): array_shifted[index] = array[value - 1]
+    return array_shifted
 
 def array_split(array):
-  l = int(len(array)/2)
-  left_split = array[:l]
-  right_split = array[l:]
-
-  return left_split, right_split
+    left_split = array[:int(len(array) / 2)]
+    right_split = array[int(len(array) / 2):]
+    return left_split, right_split
 
 def shifting_LtoR(array):
-  temp = array[0]
+    temp = array[0]
+    for index in range(1, len(array)): array[index - 1] = array[index]
+    array[len(array) - 1] = temp
+    return array
 
-  for index in range(1, len(array)):
-    array[index - 1] = array[index]
-  
-  array[len(array) - 1] = temp
-
-  return array
-
-table_p_10 = np.array([3,5,2,7,4,10,1,9,8,6])
-table_p_08 = np.array([6,3,7,4,8,5,10,9])
+table_p_10 = np.array([3, 5, 2, 7, 4, 10, 1, 9, 8, 6])
+table_p_08 = np.array([6, 3, 7, 4, 8, 5, 10, 9])
 
 key = list('0001101101')
 
-k = table_shift(key, table_p_10)
-left_split, right_split = array_split(k)
-left_split = shifting_LtoR(left_split)
-right_split = shifting_LtoR(right_split)
-key_merge = np.concatenate((left_split, right_split))
-key_1 = table_shift(key_merge, table_p_08)
-print("key 01: ", "".join([str(elem) for elem in key_1]))
+def split_and_merge(key):
+    left_split, right_split = array_split(key)
+    return np.concatenate((shifting_LtoR(left_split), shifting_LtoR(right_split)))
 
-left_spl, right_spl = array_split(key_1)
+def key_generation_1(key, table):
+    k = table_shift(key, table)
+    key_merge = split_and_merge(k)
+    return table_shift(key_merge, table)
 
-left_spl = shifting_LtoR(left_spl)
-right_spl = shifting_LtoR(right_spl)
+def key_generation_2(key, table): return split_and_merge(key)
 
-key_2 = np.concatenate((left_spl, right_spl))
-print("key 02: ", "".join([str(elem) for elem in key_2]))
+key_1 = key_generation_1(key, table_p_10)
+print("".join([str(elem) for elem in key_1]))  #1000111010
+
+key_2 = key_generation_2(key_1, table_p_08)
+print("".join([str(elem) for elem in key_2]))  #0001110101
